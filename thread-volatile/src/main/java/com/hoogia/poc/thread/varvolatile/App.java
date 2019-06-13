5473a338c3feaebd86702ba6ae3a1dccd0abf666
yangyihao <iyihao@outlook.com>
2019-06-13 18:13:56 +0800

    Improvements.

--- thread-volatile/src/main/java/com/hoogia/poc/thread/varvolatile/App.java
+++ thread-volatile/src/main/java/com/hoogia/poc/thread/varvolatile/App.java
@@ -10,13 +10,17 @@ package com.hoogia.poc.thread.varvolatile;
  */
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         VarInvisible vi = new VarInvisible();
         vi.start();
         new Thread(() -> {
             vi.setIsIdle(true);
             System.out.println("vi.idle=" + vi.isIdle());
         }).start();
+
+        Thread.sleep(10000);
+        System.out.println("10 seconds elapse, terminate myself.");
+        System.exit(0);
     }
 
 }
